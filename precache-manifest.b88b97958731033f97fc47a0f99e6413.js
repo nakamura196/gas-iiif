ad113d9fcde0fa6cf9892cd10c3fde699c6e92b0
Satoru Nakamura <na.kamura.1263@gmail.com>
2019-09-13 14:20:52 +0900

    Updates

---
+++ precache-manifest.b88b97958731033f97fc47a0f99e6413.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96293865f1738e28ae82fd47e1dd411a",
+    "url": "/gas-iiif/index.html"
+  },
+  {
+    "revision": "9752c7b37eafd55bd12a",
+    "url": "/gas-iiif/static/css/main.0dcb9c6e.chunk.css"
+  },
+  {
+    "revision": "e6c0fc5bd3ffd35afd8e",
+    "url": "/gas-iiif/static/js/2.b5a941cf.chunk.js"
+  },
+  {
+    "revision": "9752c7b37eafd55bd12a",
+    "url": "/gas-iiif/static/js/main.adb6e597.chunk.js"
+  },
+  {
+    "revision": "f708af822dfcedb3aa34",
+    "url": "/gas-iiif/static/js/runtime~main.82c9b7d7.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/gas-iiif/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/gas-iiif/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/gas-iiif/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/gas-iiif/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/gas-iiif/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/gas-iiif/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/gas-iiif/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/gas-iiif/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/gas-iiif/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/gas-iiif/static/media/nucleo-icons.f82ec6ba.ttf"
+  }
+]);
